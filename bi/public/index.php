1c4531ab47f32aaab76e6bf4d30d58e3e490095d
Juliano Silva <juliano.sil6@gmail.com>
2024-07-20 22:53:13 -0300

    ajustado namespace das rotas

--- bi/public/index.php
+++ bi/public/index.php
@@ -7,13 +7,15 @@ require_once "../vendor/autoload.php";
 $router = new Router();
 
 // Define o namespace base para as rotas
-$router->addNamespace('App\Controllers');
+$router->setNamespace('Sys\Bi\Controllers');
 
 // Adicione as rotas que apontam para os métodos correspondentes do UserController
 $router->addRoute('GET', '/cusro-fullstack-esaf/bi/public/user/:id', 'Usuarios@getUser');
 $router->addRoute('POST', '/cusro-fullstack-esaf/bi/public/user', 'Usuarios@createUser');
 $router->addRoute('PUT', '/cusro-fullstack-esaf/bi/public/user/:id', 'Usuarios@updateUser');
 $router->addRoute('DELETE', '/cusro-fullstack-esaf/bi/public/user/:id', 'Usuarios@deleteUser');
+
+$router->addRoute('GET', '/cusro-fullstack-esaf/bi/public/afazeres', 'Afazeres@index');
 
 $router->addRoute('GET', '/cusro-fullstack-esaf/bi/public/user/:id', function($id){
     echo 'teste - ' . $id;

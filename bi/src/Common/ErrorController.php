32097534115cc2c0bf379da5cc3f09dd449eb41b
Juliano Silva <juliano.sil6@gmail.com>
2024-07-20 22:52:54 -0300

    adicionados templates para paginas de erro

--- bi/src/Common/ErrorController.php
+++ bi/src/Common/ErrorController.php
@@ -5,14 +5,14 @@ namespace Sys\Bi\Common;
  * Classe ErrorController
  * @package Sys\Bi\Common
  */
-class ErrorController {
+class ErrorController extends BaseController {
 
     /**
      * Lida com erros 404 Not Found.
      */
     public function notFound() {
         http_response_code(404);
-        echo "404 Not Found";
+        echo $this->twig('errors/404.twig');
     }
 
 }
